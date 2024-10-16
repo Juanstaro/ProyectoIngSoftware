74033429bed46771b93aedf6ce08914b38856136
diego <garciadiego96@example.com>
2024-10-15 19:59:21 -0500

    (nuevo:nueva estructura)

---
+++ GestionEquipoFutbol/src/main/java/co/edu/ucentral/GestionEquipoFutbol/GestionEquipoFutbol/Controlador/Jugador.java
@@ -0,0 +1,9 @@
+package co.edu.ucentral.GestionEquipoFutbol.GestionEquipoFutbol.Controlador;
+
+public class Jugador {
+    private String nombre;
+    private String id_jugador;
+    private String Contraseña;
+    private String Fecha_registro;
+    private String Estado_Membresia;
+}
---
+++ GestionEquipoFutbol/src/main/java/co/edu/ucentral/GestionEquipoFutbol/GestionEquipoFutbol/GestionEquipoFutbolApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.GestionEquipoFutbol.GestionEquipoFutbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEquipoFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEquipoFutbolApplication.class, args);
+	}
+
+}
